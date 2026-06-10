2bc1efebcc80cf56254c865890491e49d38ddc81
syndiajean <syndiajean1@gmail.com>
2026-06-10 13:51:49 -0400

    Docs: Ajout de l'entête du module opensky.py

---
+++ airnoisepy/flight/opensky.py
@@ -0,0 +1,5 @@
+"""
+airnoisepy.flight.opensky : Récupération et nettoyage des trajectores ADS-B depuis OpenSky Network
+Responsable : Syndia Jean
+Référence : opensky-network.org et ECAC Doc 29, vol 2 - segmentation de trajectoire
+"""
